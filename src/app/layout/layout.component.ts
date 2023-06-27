89f0ee13645585e7dd9aed4e7f2e2f71f1e95651
Orlando Muñoz <orlando.munoz@ambarseguros.mx>
2023-06-27 12:35:54 -0600

    fix

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http/http.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  constructor(private httpService: HttpService) {}
+
+  async ngOnInit(): Promise<void> {
+    const response = await this.httpService.get('/projects');
+    console.log(response);
+  }
 
 }
